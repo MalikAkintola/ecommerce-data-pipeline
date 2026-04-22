77b4fb60d3bca3172ed582a733c07ba2b2311bff
Malik Akintola <107203836+MalikAkintola@users.noreply.github.com>
2026-04-22 19:24:17 +0000

    Updated scraper to dump dataset in s3

--- datasets/scraper.py
+++ datasets/scraper.py
@@ -1,5 +1,6 @@
 import kaggle
 import os
+import boto3
 
 #authenticates the kaggle.json file
 kaggle.api.authenticate()
@@ -13,3 +14,15 @@ print(f"Number of CSV files downloaded: {len(csv_files)}")
 print("\nFiles:")
 for f in csv_files:
     print(f" - {f}")
+
+
+# Upload to S3
+s3 = boto3.client('s3')
+bucket_name = 'netflixdataset-malikola'
+
+print("\nUploading to S3...")
+for f in csv_files:
+    s3.upload_file(f, bucket_name, f"raw/olist/{f}")
+    print(f" - Uploaded: {f}")
+
+print("\nAll files uploaded successfully.")
