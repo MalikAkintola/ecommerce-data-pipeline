ee61eeabb43bf88a9d3444c8ec16afbaaef26adf
Malik Akintola <107203836+MalikAkintola@users.noreply.github.com>
2026-04-22 10:26:18 +0000

    Updated scraper to show files downloaded

--- datasets/scraper.py
+++ datasets/scraper.py
@@ -1,5 +1,15 @@
 import kaggle
+import os
 
+#authenticates the kaggle.json file
 kaggle.api.authenticate()
-
+print('Kaggle Credentials authenticated. \n Proceeding to download datasets...')
+    
 kaggle.api.dataset_download_files('olistbr/brazilian-ecommerce', path = '.', unzip = True)
+
+csv_files = [f for f in os.listdir('.') if f.endswith('.csv')]
+
+print(f"Number of CSV files downloaded: {len(csv_files)}")
+print("\nFiles:")
+for f in csv_files:
+    print(f" - {f}")
