36508849c13c69ac2bfc704ee394d9f42a7c104b
Malik Akintola <107203836+MalikAkintola@users.noreply.github.com>
2026-04-27 14:28:34 +0000

    removed duplicate rows

--- olist_ecommerce_analytics/models/staging/stg_geolocation.sql
+++ olist_ecommerce_analytics/models/staging/stg_geolocation.sql
@@ -7,10 +7,19 @@ with raw_geolocation as (
         geolocation_state
     from {{ source('raw', 'olist_geolocation') }}
 )
-select
-    geolocation_zip_code_prefix as zip_code,
-    geolocation_lat as latitude,
-    geolocation_lng as longitude,
-    geolocation_city as city,
-    geolocation_state as state
-from raw_geolocation
+select 
+        zip_code,
+        latitude,
+        longitude,
+        city,
+        state
+    from (select
+            geolocation_zip_code_prefix as zip_code,
+            geolocation_lat as latitude,
+            geolocation_lng as longitude,
+            geolocation_city as city,
+            geolocation_state as state,
+            row_number() over(partition by city, state order by latitude) as rownum
+            from raw_geolocation) p
+where rownum = 1
+
