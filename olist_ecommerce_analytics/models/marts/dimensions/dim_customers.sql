95bab17489c33186bd79ad5db460132b1ff092c3
Malik Akintola <107203836+MalikAkintola@users.noreply.github.com>
2026-04-27 14:50:20 +0000

    uploaded customers table

---
+++ olist_ecommerce_analytics/models/marts/dimensions/dim_customers.sql
@@ -0,0 +1,26 @@
+with stg_customers as (
+    select 
+        customer_id,
+        customer_unique_id,
+        zip_code,
+        city,
+        state
+    from {{ source('staging', 'customers') }}
+), stg_geolocation as 
+    (select 
+        zip_code,
+        latitude,
+        longitude,
+        city,
+        state
+    from {{ source('staging', 'geolocation') }})
+select 
+        c.customer_id,
+        c.customer_unique_id,
+        c.city,
+        c.state,
+        g.longitude,
+        g.latitude
+from stg_customers c
+left join stg_geolocation g
+on c.city = g.city
