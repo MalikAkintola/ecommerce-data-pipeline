8bf69232d1b5e7b94e505cb652788d95b5cb1278
Malik Akintola <107203836+MalikAkintola@users.noreply.github.com>
2026-04-27 15:08:02 +0000

    Uploaded sellers dimensional table script

---
+++ olist_ecommerce_analytics/models/marts/dimensions/dim_sellers.sql
@@ -0,0 +1,28 @@
+{{ config(materialized='table') }}
+
+with stg_customers as (
+    select 
+    seller_id,
+    zip_code,
+    city,
+    state
+    from {{ ref('stg_sellers') }}
+), stg_geolocation as 
+    (select 
+        zip_code,
+        latitude,
+        longitude,
+        city,
+        state
+    from {{ ref('stg_geolocation') }}
+)
+select 
+        s.seller_id,
+        s.zip_code,
+        s.city,
+        s.state
+        g.longitude,
+        g.latitude
+from stg_sellers s
+left join stg_geolocation g
+on s.city = g.city and s.state = g.state
